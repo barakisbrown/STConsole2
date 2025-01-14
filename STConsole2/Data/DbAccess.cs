e00c3fa8c548a9879ad35d1627c615c17eb6a84e
barakis <lokislayer@gmail.com>
2025-01-14 16:03:20 -0600

    Added Inital Steps to start relearning c# / ado.net / SANITY

---
+++ STConsole2/Data/DbAccess.cs
@@ -0,0 +1,39 @@
+﻿namespace STConsole2.Data;
+
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Spectre.Console;
+
+
+internal class DbAccess
+{
+    private IConfiguration Configuration { get; }
+    private string connectionString { get; set; }
+
+    internal DbAccess()
+    {
+       Configuration =  new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        connectionString = Configuration.GetConnectionString("DefaultConnection");
+    }
+
+    public void DbTest()
+    {
+        AnsiConsole.Markup("[underline red]Welcome[/] [white]to Sugar Tracker Console[/]");
+        AnsiConsole.WriteLine();
+        AnsiConsole.Markup("[blink red]Opening Connection[/]");
+        AnsiConsole.WriteLine();
+        using var conn = new SqlConnection(connectionString);
+
+        if (conn.State != System.Data.ConnectionState.Open)
+        {
+            
+            conn.Open();
+        }
+
+        AnsiConsole.Markup("[blink red]Connected[/]");
+    }
+}
---
+++ STConsole2/Model/Reading.cs
@@ -0,0 +1,14 @@
+﻿namespace STConsole2.Model
+{
+    internal class Reading
+    {
+        public int ID { get; set; }
+        public int Amount { get; set; }
+        public DateOnly Added { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID = {ID}, Amount = {Amount}, Date Added = {Added}";
+        }
+    }
+}
