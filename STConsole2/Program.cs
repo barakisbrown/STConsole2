2b82f75a2aa2121a6a6cc3f5c1edf99644cb8212
barakis <lokislayer@gmail.com>
2025-01-17 01:05:42 -0600

    The program class use the Menu.GetMenuAndSelection() and then deletegates the app to various functions to perform it.

--- STConsole2/Program.cs
+++ STConsole2/Program.cs
@@ -1,4 +1,36 @@
-﻿using STConsole2.Data;
+﻿using Spectre.Console;
+using STConsole2.Data;
+using STConsole2.View;
 
+string notAvailYet = "[blink] Not Avaiable Yet[/]";
 var db = new DbAccess();
 db.DbSetup();
+
+int choice = -1;
+while(choice != 0)
+{
+    AnsiConsole.Clear();
+    choice = Menu.GetMenuAndSelection();
+    switch (choice)
+    {
+        case 0:
+            break;
+        case 1:
+            AnsiConsole.Markup(notAvailYet);
+            break;
+        case 2:
+            Menu.Add();
+            break;
+        case 3:
+            Menu.Delete();
+            break;
+        case 4:
+            Menu.Update(); break;
+        case 5:
+        case 6:
+        case 7:
+            AnsiConsole.Markup(notAvailYet);
+            break;
+    }
+}
+ 
