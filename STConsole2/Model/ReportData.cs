45bd15cbb76b73c7bc85d6c7283a774d691d35a2
barakis <lokislayer@gmail.com>
2025-01-16 21:21:43 -0600

    Class to give me a conside view of totals

---
+++ STConsole2/Model/ReportData.cs
@@ -0,0 +1,20 @@
+﻿namespace STConsole2.Model
+{
+    /// <summary>
+    /// ReportData Class will be used in a Report mode of the application.
+    /// It will show me a consise view of the following:
+    /// Number of Readings
+    /// Minimum Reading
+    /// Maximum Reading
+    /// Running AVERAGE 
+    /// Number Readings over 200
+    /// </summary>
+    internal class ReportData
+    {
+        public int Count { get; set; }
+        public int MIN { get; set; }
+        public int Max { get; set; }
+        public int AVG { get; set; }
+        public int Over200 { get; set; }
+    }
+}
